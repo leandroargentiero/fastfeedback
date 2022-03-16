671c26dd3befc30926dd13df14a217f340c9bc6b
Leandro Argentiero <argentiero.leo@gmail.com>
2022-03-16 10:02:14 +0100

    Upgraded global styles to V1

--- pages/_app.js
+++ pages/_app.js
@@ -1,37 +1,12 @@
-import { ChakraProvider, CSSReset } from '@chakra-ui/react';
-import { Global, css } from '@emotion/react';
+import { ChakraProvider } from '@chakra-ui/react';
 
 import { AuthProvider } from '@/lib/auth';
 import customTheme from '@/styles/theme';
-
-const GlobalStyle = ({ children }) => {
-  return (
-    <>
-      <CSSReset />
-      <Global
-        styles={css`
-          html {
-            min-width: 300px;
-            scroll-behaviour: smooth-scroll;
-          }
-
-          #__next {
-            display: flex;
-            flex-direction: column;
-            min-height: 100vh;
-          }
-        `}
-      />
-      {children}
-    </>
-  );
-};
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={customTheme}>
       <AuthProvider>
-        <GlobalStyle />
         <Component {...pageProps} />
       </AuthProvider>
     </ChakraProvider>
--- styles/theme.js
+++ styles/theme.js
@@ -1,6 +1,20 @@
 import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
+  styles: {
+    global: {
+      'html, body': {
+        height: '100vh',
+        backgroundColor: 'gray.50',
+        scrollBahaviour: 'smooth-scroll'
+      },
+      '#__next': {
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh'
+      }
+    }
+  },
   fonts: {
     body: `Inter,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`
   },
