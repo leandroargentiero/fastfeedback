d2acab451dc3d45933df58f9e1b8f2f884d9ebbb
Leandro Argentiero <argentiero.leo@gmail.com>
2022-03-17 12:50:45 +0100

    cleanup

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,18 +1,11 @@
-import Head from 'next/head';
-import { Button, Flex } from '@chakra-ui/react';
-import { ArrowForwardIcon } from '@chakra-ui/icons';
-
 import useSWR from 'swr';
 
 import DashboardShell from '@/components/DashboardShell';
-import EmptyState from '@/components/EmptyState';
 import fetcher from '@/utils/fetcher';
 import SiteTableSkeleton from '@/components/SiteTableSkeleton';
-import { useAuth } from '@/lib/auth';
 import SiteTable from '@/components/SiteTable';
 
 export default function Index() {
-  const auth = useAuth();
   const { data } = useSWR('/api/sites', fetcher);
 
   if (!data) {
