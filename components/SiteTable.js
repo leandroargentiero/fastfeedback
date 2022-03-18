2795359be0de37c9e278be9a3db26a0dd184ffa5
Leandro Argentiero <argentiero.leo@gmail.com>
2022-03-18 11:03:17 +0100

    Created links that point to corresponding dynamic route

--- components/SiteTable.js
+++ components/SiteTable.js
@@ -1,6 +1,6 @@
-import { Box } from '@chakra-ui/react';
+import NextLink from 'next/link';
+import { Box, Link } from '@chakra-ui/react';
 import { format, parseISO } from 'date-fns';
-import Link from 'next/link';
 import { Table, Tr, Th, Td } from './Table';
 
 const SiteTable = ({ sites }) => {
@@ -22,7 +22,9 @@ const SiteTable = ({ sites }) => {
               <Td fontWeight="medium">{site.name}</Td>
               <Td>{site.url}</Td>
               <Td>
-                <Link href="#">View Feedback</Link>
+                <NextLink href={`/p/${site.id}`} passHref>
+                  <Link>View Feedback</Link>
+                </NextLink>
               </Td>
               <Td>{format(parseISO(site.createdAt), 'PPpp')}</Td>
             </Box>
