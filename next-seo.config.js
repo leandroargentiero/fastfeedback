4c61d04270ae1b5f9b6f7ae6740e5711969b493f
Leandro Argentiero <argentiero.leo@gmail.com>
2022-03-22 17:47:19 +0100

    Added SEO with next-seo

---
+++ next-seo.config.js
@@ -0,0 +1,32 @@
+const title =
+  'Fast Feedback - The easiest way to add comments to your static site.';
+const description =
+  'This version of Fast Feedback was built by Leandro Argentiero as part of React 2025.';
+
+const SEO = {
+  title,
+  description,
+  canonical: 'https://fastfeedback-sandy-sigma.vercel.app/',
+  openGraph: {
+    type: 'website',
+    locale: 'en_IE',
+    url: 'https://fastfeedback-sandy-sigma.vercel.app',
+    title,
+    description,
+    images: [
+      {
+        url: 'https://fastfeedback-sandy-sigma.vercel.app/og.jpg',
+        alt: title,
+        width: 1280,
+        height: 720
+      }
+    ]
+  },
+  twitter: {
+    handle: '',
+    site: '',
+    cardType: 'summary_large_image'
+  }
+};
+
+export default SEO;
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { DefaultSeo } from 'next-seo';
 
 import { AuthProvider } from '@/lib/auth';
 import customTheme from '@/styles/theme';
+import SEO from 'next-seo.config';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={customTheme}>
       <AuthProvider>
+        <DefaultSeo {...SEO} />
         <Component {...pageProps} />
       </AuthProvider>
     </ChakraProvider>
