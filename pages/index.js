02fc3943955722991c2c95da2df659d5273e4ae0
Leandro Argentiero <argentiero.leo@gmail.com>
2022-03-22 14:35:14 +0100

    UI polishing

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
 import Head from 'next/head';
-import { Button, Flex, Text } from '@chakra-ui/react';
-import { ArrowForwardIcon } from '@chakra-ui/icons';
+import NextLink from 'next/link';
+import {
+  Button,
+  Flex,
+  Text,
+  Link,
+  Box,
+  Heading,
+  VStack
+} from '@chakra-ui/react';
+import { FaGithub } from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc';
 
 import { useAuth } from '@/lib/auth';
 import { LogoIcon } from '@/components/CustomIcons';
-import Link from 'next/link';
 
 export default function Index() {
   const auth = useAuth();
@@ -29,31 +38,73 @@ export default function Index() {
           }}
         />
       </Head>
+      <VStack
+        bgColor="white"
+        px={5}
+        py={16}
+        borderRadius={4}
+        border="1px solid"
+        borderColor="gray.100"
+      >
+        <Box textAlign="center">
+          <LogoIcon color="black" w={16} h={16} mb={4} />
+          <Heading size="xl">Log in to Fast Feedback </Heading>
+          <Text fontSize="sm" textAlign="center" maxWidth="450px" my={4}>
+            This version of {''}
+            Fast Feedback was built by{' '}
+            <Link
+              textDecoration="underline"
+              _hover={{ textDecoration: 'none' }}
+            >
+              Leandro Argentiero
+            </Link>{' '}
+            as part of React 2025. It's an easy solution for adding comments or
+            reviews to your static site. It's still a work in progress, but you
+            can try it out by logging in.
+          </Text>
+        </Box>
 
-      <LogoIcon color="black" w={16} h={16} />
-
-      <Text fontSize="sm" textAlign="center" maxWidth="450px" my={4}>
-        Fast Feedback is being built as part of React 2025. It's an easy
-        solution for adding comments or reviews to your static site. It's still
-        a work in progress, but you can try it out by logging in.
-      </Text>
-
-      {auth.user ? (
-        <Link href="/dashboard" passHref>
-          <Button as="a" size="sm" mt={4}>
-            Go to dashboard →
-          </Button>
-        </Link>
-      ) : (
-        <Button
-          onClick={(e) => auth.signinWithGitHub()}
-          rightIcon={<ArrowForwardIcon />}
-          size="sm"
-          mt={4}
-        >
-          Sign in with Github
-        </Button>
-      )}
+        {auth.user ? (
+          <NextLink href="/dashboard" passHref>
+            <Button
+              size="md"
+              mt={4}
+              backgroundColor="gray.900"
+              color="white"
+              fontWeight="medium"
+              _hover={{ bg: 'gray.700' }}
+              _active={{ bg: 'gray.800', transform: 'scale(0.98)' }}
+            >
+              Go to dashboard
+            </Button>
+          </NextLink>
+        ) : (
+          <VStack>
+            <Button
+              onClick={(e) => auth.signinWithGitHub()}
+              leftIcon={<FaGithub />}
+              size="md"
+              mt={4}
+              backgroundColor="gray.900"
+              color="white"
+              fontWeight="medium"
+              _hover={{ bg: 'gray.700' }}
+              _active={{ bg: 'gray.800', transform: 'scale(0.98)' }}
+            >
+              Continue with Github
+            </Button>
+            <Button
+              onClick={(e) => auth.signinWithGoogle()}
+              leftIcon={<FcGoogle />}
+              size="md"
+              mt={4}
+              variant="outline"
+            >
+              Continue with Google
+            </Button>
+          </VStack>
+        )}
+      </VStack>
     </Flex>
   );
 }
