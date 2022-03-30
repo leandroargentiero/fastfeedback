6828fda5489d85c7a257ba55d34f41fee9d33516
Leandro Argentiero <argentiero.leo@gmail.com>
2022-03-30 11:33:34 +0200

    Editing site directly mutates feedback result

--- pages/sites/[siteId].js
+++ pages/sites/[siteId].js
@@ -1,108 +1,108 @@
-import { useRef, useState } from 'react';
+import useSWR from 'swr';
 import { useRouter } from 'next/router';
+import { useForm } from 'react-hook-form';
 import {
-  FormControl,
-  FormLabel,
   Box,
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
   Textarea,
-  Button
+  useToast
 } from '@chakra-ui/react';
 
+import fetcher from '@/utils/fetcher';
 import { useAuth } from '@/lib/auth';
-import { createFeedback, getSite } from '@/lib/db';
-import { getAllFeedback, getAllSites } from '@/lib/db-admin';
+import { createFeedback } from '@/lib/db';
 import Feedback from '@/components/Feedback';
 import DashboardShell from '@/components/DashboardShell';
 import SiteFeedbackTableHeader from '@/components/SiteFeedbackTableHeader';
 
-export async function getStaticProps(context) {
-  const siteId = context.params.siteId;
-  const { feedback } = await getAllFeedback(siteId);
-  const { site } = await getSite(siteId);
-
-  return {
-    props: {
-      initialFeedback: feedback,
-      site
-    },
-    revalidate: 10
-  };
-}
-
-export async function getStaticPaths() {
-  const { sites } = await getAllSites();
-
-  const paths = sites.map((site) => ({
-    params: {
-      siteId: site.id.toString()
-    }
-  }));
-
-  return {
-    paths,
-    fallback: true
-  };
-}
-
-const SiteFeedback = ({ initialFeedback = [], site }) => {
+const SiteFeedback = () => {
   const auth = useAuth();
   const router = useRouter();
-  const inputEl = useRef(null);
-  const [allFeedback, setAllFeedback] = useState(initialFeedback);
+  const toast = useToast();
+  const { siteId } = router.query;
+  const {
+    register,
+    handleSubmit,
+    resetField,
+    formState: { errors, isSubmitting }
+  } = useForm();
+  const { data: siteData } = useSWR(`/api/site/${siteId}`, fetcher);
+  const { data: feedbackData, mutate: mutateFeedbackData } = useSWR(
+    `/api/feedback/${siteId}`,
+    fetcher
+  );
 
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    const { site } = await getSite(router.query.siteId);
-
+  const onAddComment = async ({ comment }) => {
     const newFeedback = {
       author: auth.user.name,
       authorId: auth.user.uid,
-      siteId: router.query.siteId,
-      siteName: site.name,
-      text: inputEl.current.value,
+      siteId,
+      siteName: siteData.name,
+      text: comment,
       createdAt: new Date().toISOString(),
       provider: auth.user.provider,
       status: 'pending'
     };
 
-    inputEl.current.value = '';
-    const { error } = createFeedback(newFeedback);
-
-    if (!error) {
-      setAllFeedback([newFeedback, ...allFeedback]);
+    try {
+      resetField('comment');
+      await createFeedback(newFeedback);
+      mutateFeedbackData([newFeedback, ...feedbackData]);
+    } catch (error) {
+      console.log(error);
+      toast({
+        title: 'Something went wrong.',
+        description: "We've failed to add your comment",
+        status: 'error',
+        duration: 5000,
+        isClosable: true
+      });
     }
   };
 
   return (
     <DashboardShell>
-      <SiteFeedbackTableHeader site={site} isSiteOwner={true} />
+      <SiteFeedbackTableHeader site={siteData} isSiteOwner={true} />
       <Box display="flex" flexDirection="column" w="full">
-        <Box as="form" onSubmit={onSubmit} mb={16}>
-          <FormControl>
-            <FormLabel htmlFor="email">Comment</FormLabel>
+        <Box as="form" onSubmit={handleSubmit(onAddComment)} mb={16}>
+          <FormControl isInvalid={errors.comment}>
+            <FormLabel htmlFor="comment">Comment</FormLabel>
             <Textarea
-              ref={inputEl}
-              background="white"
               id="comment"
-              mb={4}
-              placeholder="Here is a sample placeholder"
+              background="white"
+              placeholder="Leave your comment..."
+              {...register('comment', {
+                required: 'Comment is required.'
+              })}
             />
-            <Button
-              type="submit"
-              isDisabled={router.isFallback}
-              size="md"
-              backgroundColor="gray.900"
-              color="white"
-              fontWeight="medium"
-              _hover={{ bg: 'gray.700' }}
-              _active={{ bg: 'gray.800', transform: 'scale(0.98)' }}
-            >
-              Add Comment
-            </Button>
+            <FormErrorMessage>
+              {errors.comment && errors.comment.message}
+            </FormErrorMessage>
           </FormControl>
+          <Button
+            type="submit"
+            isLoading={isSubmitting}
+            isDisabled={router.isFallback}
+            size="md"
+            mt={4}
+            backgroundColor="gray.900"
+            color="white"
+            fontWeight="medium"
+            _hover={{ bg: 'gray.700' }}
+            _active={{ bg: 'gray.800', transform: 'scale(0.98)' }}
+          >
+            Add Comment
+          </Button>
         </Box>
-        {allFeedback.map((feedback) => (
-          <Feedback key={feedback.createdAt} {...feedback} />
+        {feedbackData?.map((feedback) => (
+          <Feedback
+            key={feedback.createdAt}
+            siteSettings={siteData?.settings}
+            {...feedback}
+          />
         ))}
       </Box>
     </DashboardShell>
